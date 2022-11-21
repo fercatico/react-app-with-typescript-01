3363d4513051c01fc9d0bfab9fa33eed4557598a
Miguel Fernandez <miguelfera@gmail.com>
2022-11-21 16:15:09 -0600

    Functional Version

---
+++ src/components/Form.tsx
@@ -0,0 +1,101 @@
+import React, { useReducer, useState } from "react";
+import { Sub } from "../types";
+
+interface FormState {
+  inputValues: Sub;
+}
+
+interface FormProps {
+  onNewSub: (newSub: Sub) => void;
+}
+
+const INITIAL_STATE = {
+  nick: "",
+  subMonths: 0,
+  avatar: "",
+  description: "",
+};
+
+type FormReducerAction =
+  | {
+      type: "change_value";
+      payload: {
+        inputName: string;
+        inputValue: string;
+      };
+    }
+  | { type: "clear" };
+
+const formReducer = (
+  state: FormState["inputValues"],
+  action: FormReducerAction
+) => {
+  switch (action.type) {
+    case "change_value":
+      const { inputName, inputValue } = action.payload;
+      return {
+        ...state,
+        [inputName]: inputValue,
+      };
+    case "clear":
+      return INITIAL_STATE;
+  }
+};
+
+const Form = ({ onNewSub }: FormProps) => {
+  const [inputValues, setInputValues] =
+    useState<FormState["inputValues"]>(INITIAL_STATE);
+
+  const [stateValues, dispatch] = useReducer(formReducer, INITIAL_STATE);
+
+  const handleSubmit = (evt: React.FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+    onNewSub(inputValues);
+  };
+
+  const handleChange = (
+    evt: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setInputValues({ ...inputValues, [evt.target.name]: evt.target.value });
+  };
+
+  const handleClear = () => {
+    setInputValues(INITIAL_STATE);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={inputValues.nick}
+          onChange={handleChange}
+          name="nick"
+        />
+        <input
+          type="number"
+          value={inputValues.subMonths}
+          onChange={handleChange}
+          name="subMonths"
+        />
+        <input
+          type="text"
+          value={inputValues.avatar}
+          onChange={handleChange}
+          name="avatar"
+        />
+        <textarea
+          value={inputValues.description}
+          onChange={handleChange}
+          name="description"
+        />
+        <button onClick={handleClear} type="button">
+          Clear the form!
+        </button>
+        <button>Save New Sub</button>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
---
+++ src/components/List.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Sub } from "../types";
+
+interface Props {
+  subs: Array<Sub>;
+}
+
+const List = ({ subs }: Props) => {
+  const renderList = (): JSX.Element[] => {
+    return subs.map((sub) => {
+      return (
+        <li key={sub.nick}>
+          <img src={sub.avatar} alt="Avatar" />
+          <h4>
+            {sub.nick} (<small>{sub.subMonths}</small>)
+          </h4>
+          <p>{sub.description?.substring(0, 100)}</p>
+        </li>
+      );
+    });
+  };
+
+  return <ul>{renderList()}</ul>;
+};
+
+export default List;
